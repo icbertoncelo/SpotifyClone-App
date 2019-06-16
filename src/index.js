5e1b76ba61c20a6fe9e88c19985b2cfdad79b282
Ian Carlos <carlos.ian007@gmail.com>
2019-06-15 21:24:40 -0300

    Feat(project): Add project structure and initial redux configuration

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import '~/config/ReactotronConfig';
+import '~/config/StatusBarConfig';
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import Routes from '~/routes';
+import store from '~/store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    );
+  }
+}
+
+export default App;
